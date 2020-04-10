c1a6a2143d11bfb0a21f54f55861ad58bfa818af
agavrel <gavrel.antonin@gmail.com>
2020-04-10 23:22:52 +0200

    [ADD] More code snippets for reversing uinteger bits

--- 1.9_reverse_bits_assembly.cpp
+++ 1.9_reverse_bits_assembly.cpp
@@ -1,7 +1,8 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <immintrin.h>
+#include <x86intrin.h>
 // Compile with g++ -masm=intel a.cpp && ./a.out 
 
 template <class T>
@@ -18,9 +19,9 @@ T reverse_bits(T n) {
 }
 
 
-unsigned char reverse_bits3(unsigned char c) {
-    __asm__ __volatile__ (R"(
-		mov cx, 8       
+unsigned char reverse_bits1(unsigned char c) {
+	__asm__ __volatile__ (R"(
+		or cx, 8       
 	myloop:                   
 		ror di          
 		adc ax, ax      
@@ -41,8 +42,53 @@ uint32_t reverse_bits2(uint32_t a) {
 }
 
 
+uint32_t reverse_bits4(uint32_t n) {
+//	uint32_t a = n;
+// add .intel_syntax
+ // mov     edi, DWORD PTR [rbp-8] 
+	__asm__ __volatile__ (R"(
+            bswap   edi
+            mov     edx, edi
+            shr     edi, 4
+            sal     edx, 4
+            and     edi, 252645135
+            and     edx, -252645136
+            or      edi, edx
+            mov     edx, edi
+            sal     edi, 2
+            shr     edx, 2
+            and     edi, -858993460
+            and     edx, 858993459
+            or      edx, edi
+            mov     eax, edx
+            add     edx, edx
+            shr     eax
+            and     edx, -1431655766
+            and     eax, 1431655765
+            or      eax, edx
+	
+        ;)");
 
-template <class T>
+	//mov     DWORD PTR [rbp-8], eax 
+	//return a;
+}
+
+
+uint32_t reverse_bits5(uint32_t n) {
+	uint32_t a = n;
+
+	__asm__ __volatile__ (R"(
+		xor eax, eax
+		inc eax
+	myloop3:
+		shr edi, 1
+		adc eax, eax
+		jnc short myloop3
+        ;)");
+}
+
+
+	template <class T>
 void print_binary(T n)
 {	T mask = 1UL << ((sizeof(n) * 8) - 1);  // will set the most significant bit
 	for(; mask != 0; mask >>= 1) putchar('0' + !!(n & mask));
@@ -50,30 +96,30 @@ void print_binary(T n)
 }
 
 int main() {
-	unsigned char n = 103;//12534534U | 1U << 31;
-	
+	unsigned int n = 12534534U | 1U << 31;
+
 	print_binary(n);
-	unsigned char o = reverse_bits3(n);
+	unsigned int o = reverse_bits5(n);
 	print_binary(o);
 	n = reverse_bits(n);
 	print_binary(n);
 
-/*
-	uint32_t n = 12;
-	print_binary(n);
-	n = reverse_bits(n); 
-	print_binary(n);
-	unsigned char c = 'a';
-	print_binary(c);
-	c = reverse_bits(c);
-	print_binary(c);
-	uint16_t s = 12;
-	print_binary(s);
-	s = reverse_bits(s);
-	print_binary(s);
-	uint64_t l = 12;
-	print_binary(l);
-	l = reverse_bits(l);
-	print_binary(l);*/
+	/*
+	   uint32_t n = 12;
+	   print_binary(n);
+	   n = reverse_bits(n); 
+	   print_binary(n);
+	   unsigned char c = 'a';
+	   print_binary(c);
+	   c = reverse_bits(c);
+	   print_binary(c);
+	   uint16_t s = 12;
+	   print_binary(s);
+	   s = reverse_bits(s);
+	   print_binary(s);
+	   uint64_t l = 12;
+	   print_binary(l);
+	   l = reverse_bits(l);
+	   print_binary(l);*/
 	return 0;
 }
